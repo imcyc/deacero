604bd68b930aa89ef3442cea06cf6420f5e446ab
Axel <axosar@gmail.com>
2019-11-07 09:21:39 -0600

    Updates

---
+++ precache-manifest.a88b341d05d2ac36aabb20c32beebf61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e33426173a4753070a59fd0d4a1c64a",
+    "url": "/deacero/index.html"
+  },
+  {
+    "revision": "03cf53da89d83d94d8b6",
+    "url": "/deacero/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "097b5749f13bb1337247",
+    "url": "/deacero/static/js/2.966882bb.chunk.js"
+  },
+  {
+    "revision": "03cf53da89d83d94d8b6",
+    "url": "/deacero/static/js/main.269afcb3.chunk.js"
+  },
+  {
+    "revision": "50628452974f635f1911",
+    "url": "/deacero/static/js/runtime-main.fdf8e765.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/deacero/static/media/logo.25bf045c.svg"
+  }
+]);
