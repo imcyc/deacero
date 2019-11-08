80100a53ce485f9c081c745818ce361bc1d66955
Axel <axosar@gmail.com>
2019-11-08 16:49:26 -0600

    Datos otros

--- src/componentes/Contenido/Resultados.js
+++ src/componentes/Contenido/Resultados.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import data from "../../data";
 
 const Resultados = (props) => {
-  const {area, destino, longitud, concretoPrecio, mallaPrecio, tipo} = props;
+  const {area, destino, longitud, concretoPrecio, mallaPrecio, tipo, factorGlobal} = props;
+  let costo, espesor, costoTotal;
   data.tipo.map(dato => {
     if(tipo === dato.nombre){
       if(parseFloat(longitud) === dato.datos.longitud && destino === dato.datos.destino){
         console.log(dato.datos.precio);
+        costo = dato.datos.precio * factorGlobal;
+        espesor = dato.datos.espesor;
+        costoTotal = costo * area;
       }
     }else{
       console.log('naranjas');
@@ -16,26 +22,140 @@ const Resultados = (props) => {
     <div className="Resultados">
       <h1><i className="material-icons">keyboard_arrow_right</i> RESULTADOS</h1>
       <hr/>
-      <div className="datos">
-        <div>
+      {/*
+      <Row>
+        <Col>
           <h2><i className="material-icons">keyboard_arrow_right</i> Área en Mts<sup>2</sup>:</h2>
           <p>{area} Mts. <sup>2</sup></p>
           <h2><i className="material-icons">keyboard_arrow_right</i> Precio Concreto:</h2>
           <p>$ {concretoPrecio}.00 MXN</p>
-        </div>
-        <div>
+        </Col>
+        <Col>
           <h2><i className="material-icons">keyboard_arrow_right</i> Destino:</h2>
           <p>{destino}</p>
           <h2><i className="material-icons">keyboard_arrow_right</i> Precio Malla:</h2>
           <p>$ {mallaPrecio}.00 MXN</p>
-        </div>
-        <div>
+        </Col>
+        <Col>
           <h2><i className="material-icons">keyboard_arrow_right</i> Longitud:</h2>
           <p>{longitud} Mts.</p>
           <h2><i className="material-icons">keyboard_arrow_right</i> Tipo de Malla:</h2>
           <p>{tipo}</p>
-        </div>
-      </div>
+        </Col>
+      </Row>
+      <hr/>
+      */}
+      <Row className="rex tit">
+        <Col>
+          <h1><i className="material-icons">keyboard_arrow_right</i> VIG-BOV Alma Abierta</h1>
+        </Col>
+      </Row>
+      <Row className="rex">
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Espesor de la Losa:</h2>
+          <p>{espesor} cm</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Costo:</h2>
+          <p>$ {costo.toFixed(2)} por M<sup>2</sup></p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Costo Total:</h2>
+          <p>$ {costoTotal.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Cimbrado:</h2>
+          <p>1.6 Jor.</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Ejecución Total:</h2>
+          <p>4.9 Jor.</p>
+        </Col>
+      </Row>
+      <hr/>
+      <Row className="rex tit">
+        <Col>
+          <h1><i className="material-icons">keyboard_arrow_right</i> VIG-BOV Pretensada</h1>
+        </Col>
+      </Row>
+      <Row className="rex">
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Espesor de la Losa:</h2>
+          <p>{espesor} cm</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Costo:</h2>
+          <p>$ {costo.toFixed(2)} por M<sup>2</sup></p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Costo Total:</h2>
+          <p>$ {costoTotal.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Cimbrado:</h2>
+          <p>1.6 Jor.</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Ejecución Total:</h2>
+          <p>4.9 Jor.</p>
+        </Col>
+      </Row>
+      <hr/>
+      <Row className="rex tit">
+        <Col>
+          <h1><i className="material-icons">keyboard_arrow_right</i> LOSA SÓLIDA Y VAR. G42</h1>
+        </Col>
+      </Row>
+      <Row className="rex">
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Espesor de la Losa:</h2>
+          <p>{espesor} cm</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Costo:</h2>
+          <p>$ {costo.toFixed(2)} por M<sup>2</sup></p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Costo Total:</h2>
+          <p>$ {costoTotal.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Cimbrado:</h2>
+          <p>1.6 Jor.</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Ejecución Total:</h2>
+          <p>4.9 Jor.</p>
+        </Col>
+      </Row>
+      <hr/>
+      <Row className="rex tit">
+        <Col>
+          <h1><i className="material-icons">keyboard_arrow_right</i> LOSA ALIGERADA Y VAR. G42</h1>
+        </Col>
+      </Row>
+      <Row className="rex">
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Espesor de la Losa:</h2>
+          <p>{espesor} cm</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Costo:</h2>
+          <p>$ {costo.toFixed(2)} por M<sup>2</sup></p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Costo Total:</h2>
+          <p>$ {costoTotal.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Cimbrado:</h2>
+          <p>1.6 Jor.</p>
+        </Col>
+        <Col>
+          <h2><i className="material-icons">keyboard_arrow_right</i> Ejecución Total:</h2>
+          <p>4.9 Jor.</p>
+        </Col>
+      </Row>
       <hr/>
     </div>
   );
