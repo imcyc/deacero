47615e3a7e908ad90d01ffb92a16704014f63801
Axel <axosar@gmail.com>
2019-11-22 13:43:45 -0600

    App

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { HashRouter, BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import Inicio from './componentes/Inicio';
 import Dashboard from './componentes/Dashboard';
 import './App.css';
 
 function App() {
   return (
-    <HashRouter basename='/'>
-      <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
+      <Switch>
         <Route exact path="/" component={Inicio} />
         <Route exact path="/dashboard" component={Dashboard} />
-      </Router>
+      </Switch>
     </HashRouter>
   );
 }
